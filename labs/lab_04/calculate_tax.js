9d0679ae7eda522e86b38560eafbe0002c685214
drewburton <drew.burton.gc@gmail.com>
2023-09-18 12:35:51 -0400

    finished lab 4

--- labs/lab_04/calculate_tax.js
+++ labs/lab_04/calculate_tax.js
@@ -1,8 +1,55 @@
 "use strict";
+
 const $ = selector => document.querySelector(selector);
 
-document.addEventListener("DOMContentLoaded", () => {
-	// add event handlers
-	$("#calculate").addEventListener("click", processEntry);
+$("#subtotal").focus();
 
+const processEntries = () => {
+	let subtotal = parseFloat($("#subtotal").value);
+	let taxRate = parseFloat($("#tax_rate").value);
+
+	if (subtotal < 0 || subtotal > 10000 || isNaN(subtotal)) {
+		alert("Subtotal must be > 0 and < 10000");
+		$("#subtotal").value = "";
+		$("#subtotal").focus();
+		return;
+	}
+	if (taxRate < 0 || taxRate > 12 || isNaN(taxRate)) {
+		alert("Tax Rate must be > 0 and < 12");
+		$("#tax_rate").value = "";
+		$("#tax_rate").focus();
+		return;
+	}
+
+	let salesTax = subtotal * (taxRate / 100);
+	let total = subtotal + salesTax;
+
+	$("#tax").value = salesTax.toFixed(2);
+	$("#total").value = total.toFixed(2);
+
+	$("#subtotal").focus();
+};
+
+const clear = () => {
+	clearSubtotal();
+	clearTaxRate();
+	$("#tax").value = "";
+	$("#total").value = "";
+
+	$("#subtotal").focus();
+};
+
+const clearSubtotal = () => {
+	$("#subtotal").value = "";
+};
+
+const clearTaxRate = () => {
+	$("#tax_rate").value = ""
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	$("#calculate").addEventListener("click", processEntries);
+	$("#clear").addEventListener("click", clear);
+	$("#subtotal").addEventListener("click", clearSubtotal);
+	$("#tax_rate").addEventListener("click", clearTaxRate);
 });
