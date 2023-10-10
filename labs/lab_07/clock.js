97436de197aa07f3207fa8f4ef45c5313dbf3c2d
drewburton <drew.burton.gc@gmail.com>
2023-10-10 19:38:23 -0400

    finished lab 8

--- labs/lab_07/clock.js
+++ labs/lab_07/clock.js
@@ -32,39 +32,65 @@ let elapsedMilliseconds = 0;
 
 const tickStopwatch = () => {
     // increment milliseconds by 10 milliseconds
+    elapsedMilliseconds += 10;
 
     // if milliseconds total 1000, increment seconds by one and reset milliseconds to zero
+    if (elapsedMilliseconds >= 1000) {
+        elapsedMilliseconds -= 1000;
+        elapsedSeconds += 1;
+    }
 
     // if seconds total 60, increment minutes by one and reset seconds to zero
+    if (elapsedSeconds >= 60) {
+        elapsedSeconds -= 60;
+        elapsedMinutes += 1;
+    }
 
     //display new stopwatch time
-
+    $("#s_minutes").textContent = padSingleDigit(elapsedMinutes);
+    $("#s_seconds").textContent = padSingleDigit(elapsedSeconds);
+    $("#s_ms").textContent = elapsedMilliseconds.toString().padStart(3, "0");
 };
 
 // event handler functions
 const startStopwatch = evt => {
     // prevent default action of link
+    evt.preventDefault();
 
     // do first tick of stop watch and then set interval timer to tick
     // stop watch every 10 milliseconds. Store timer object in stopwatchTimer
     // variable so next two functions can stop timer.
-
+    if (!stopwatchTimer) {
+        tickStopwatch();
+        stopwatchTimer = setInterval(tickStopwatch, 10);
+    }
 };
 
 const stopStopwatch = evt => {
     // prevent default action of link
+    evt.preventDefault();
 
     // stop timer
-
+    clearInterval(stopwatchTimer);
+    stopwatchTimer = null;
 };
 
 const resetStopwatch = evt => {
     // prevent default action of link
+    evt.preventDefault();
 
     // stop timer
+    clearInterval(stopwatchTimer);
+    stopwatchTimer = null;
 
     // reset elapsed variables and clear stopwatch display
+    elapsedMinutes = 0;
+    elapsedSeconds = 0;
+    elapsedMilliseconds = 0;
 
+    $("#s_minutes").textContent = padSingleDigit(elapsedMinutes);
+    $("#s_seconds").textContent = padSingleDigit(elapsedSeconds);
+    $("#s_ms").textContent = elapsedMilliseconds.toString().padStart(3, "0");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -75,4 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(displayCurrentTime, 1000);
 
     // set up stopwatch event handlers
+    $("#start").addEventListener("click", startStopwatch);
+    $("#stop").addEventListener("click", stopStopwatch);
+    $("#reset").addEventListener("click", resetStopwatch);
 });
