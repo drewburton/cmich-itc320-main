88ddcf3282306ca0c62d6d833644264b5d1ebd84
drewburton <drew.burton.gc@gmail.com>
2023-11-02 09:54:07 -0400

    finished lab 12

--- labs/lab_12/password.js
+++ labs/lab_12/password.js
@@ -1,29 +1,39 @@
 "use strict";
 
 const getRandomNumber = max => {
-	let random = null;
-	if (!isNaN(max)) {
-		random = Math.random();             // value >= 0.0 and < 1.0
-		random = Math.floor(random * max);  // value is an integer between 0 and max - 1
-		random = random + 1;                // value is an integer between 1 and max
-	}
-	return random;
+    let random = null;
+    if (!isNaN(max)) {
+        random = Math.random();             // value >= 0.0 and < 1.0
+        random = Math.floor(random * max);  // value is an integer between 0 and max - 1
+        random = random + 1;                // value is an integer between 1 and max
+    }
+    return random;
 };
 
-$(document).ready( () => {
-    $("#generate").click( () => {
-        $("#password").val( "" ); // clear previous entry
-    
-        const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@";
-        
+$(document).ready(() => {
+    $("#generate").click(() => {
+        const input = $("#num").val();
+        $("#password").val(""); // clear previous entry
+
+        if (isNaN(input)) {
+            alert("Please enter a valid number");
+        } else {
+            const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@";
+            let password = "";
+            for (let i = 0; i < parseInt(input); i++) {
+                let char_index = Math.floor(Math.random() * chars.length);
+                password += chars[char_index];
+            }
+            $("#password").val(password);
+        }
     }); // end click()
-    
-    $("#clear").click( () => {
-        $("#num").val( "" );
-        $("#password").val( "" );
+
+    $("#clear").click(() => {
+        $("#num").val("");
+        $("#password").val("");
         $("#num").focus();
     }); // end click()
-    
+
     // set focus on initial load
     $("#num").focus();
 }); // end ready()
